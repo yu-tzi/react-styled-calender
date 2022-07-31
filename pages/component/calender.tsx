970e85d87075bba8fb14eb94de992cf58c83102b
Kiki <zoeluoluo05673@gmail.com>
2022-07-31 21:07:27 +0800

    feat: mapping this month date options

--- pages/component/calender.tsx
+++ pages/component/calender.tsx
@@ -1,10 +1,48 @@
-const Calender = (defautTime: any, setDefaultTime: void) => {
-    // 當下的年份 月份
-    // 字串 from mon to sun
-    // 這個月的所有日期按照星期排列
-    console.log(defautTime);
+import dayjs from 'dayjs'
+import { useEffect } from 'react'
+
+// setting dayjs plugin
+var toObject = require('dayjs/plugin/toObject')
+dayjs.extend(toObject)
+
+
+const Calender = (props) => {
+    const { defautTime, setDefaultTime } = props
+    const { years, months, date } = defautTime || {}
+    const firstDay = dayjs().startOf('month').day()
+    const lastDate = dayjs().endOf('month').date()
+    const weekList = [ 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun' ]
+    const monthDay = []
+    for(let i=1; i <= firstDay + lastDate; i++){
+        if(i <= firstDay){
+            monthDay.push(0);
+        }else{
+            monthDay.push(i - firstDay);
+        }
+    }
+
+    useEffect(()=>{
+        // @ts-ignore: Unreachable code error
+        const now = dayjs().toObject();
+        setDefaultTime(now)
+      },[])
+
     return(
-        <div>hi</div>
+        <>
+            <div>{`${years} 年 ${months} 月`}</div>
+            <div>
+                {weekList.map((day)=>{
+                    return(
+                        <div key={`day-key-${day}`}>{day}</div>
+                    )
+                })}
+                {monthDay.map((date, index)=>{
+                    return(
+                        <div key={`date-key-${index}`}>{date}</div>
+                    )
+                })}
+            </div>
+        </>
     )
 }
 export default Calender
--- pages/component/index.tsx
+++ pages/component/index.tsx
@@ -1,19 +1,10 @@
 import Calender from "./calender"
 import Input from "./input"
 import { useState, useEffect } from 'react'
-import dayjs from 'dayjs'
 
-// setting dayjs plugin
-var toObject = require('dayjs/plugin/toObject')
-dayjs.extend(toObject)
 
 const DatePicker = () => {
   const [defautTime, setDefaultTime] = useState(null);
-  useEffect(()=>{
-    // @ts-ignore: Unreachable code error
-    const now = dayjs().toObject();
-    setDefaultTime(now)
-  },[])
 
     return (
         <>
