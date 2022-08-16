5b54a111bc3d30a489da03b96acc98d04db0d502
Kiki <zoeluoluo05673@gmail.com>
2022-08-17 00:16:57 +0800

    feat: 更新 next config 檔案

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true
+  },
   experimental: {
     // Enables the styled-components SWC transform
-    styledComponents: true
+    
   }
 }
 
